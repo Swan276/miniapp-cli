39cc1057b8e601ea52f8d06bd88587db76003d0b
Swan☝️🤓 <swansaung.lwin@wavemoney.com.mm>
2024-05-21 08:54:18 +0630

    version number added

--- cmd/root.go
+++ cmd/root.go
@@ -27,13 +27,8 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "miniapp-cli",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Cli tool to build MiniApps",
+	Long:  `Cli tool to build MiniApps. Version 0.1.2`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
